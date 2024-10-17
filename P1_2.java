c99b3421f827cb6eaf8d2961516e214405bd94bd
D24DCS178 <d24dcs178@charusat.edu.in>
2024-10-17 23:23:26 +0530

    Delete P1_2.java

--- P1_2.java
+++
@@ -1,7 +0,0 @@
-public class P1_2 {
-    public static void main(String[] args) {
-            double currentbal = 20.0;
-    
-            System.out.println("Current Balance := "+currentbal);
-    }
-}
